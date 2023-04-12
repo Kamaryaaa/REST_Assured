9bef393a5e2bed61e94f1882dbc5479298c67547
kamaryaa <kamaryaa12@gmail.com>
2023-04-11 22:56:32 -0400

    UPDATE

--- src/test/java/com/cydeo/day10/P04_JsonSchemaValidation.java
+++ src/test/java/com/cydeo/day10/P04_JsonSchemaValidation.java
@@ -3,7 +3,12 @@ package com.cydeo.day10;
 import com.cydeo.utilities.SpartanTestBase;
 import io.restassured.http.ContentType;
 import io.restassured.module.jsv.JsonSchemaValidator;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 
@@ -27,6 +32,8 @@ public class P04_JsonSchemaValidation extends SpartanTestBase {
 
     }
 
+
+    @DisplayName("GET /api/spartans/search to validate with JsonSchemaValidator")
     @Test
     public void test2(){
         given().accept(ContentType.JSON)
@@ -34,6 +41,57 @@ public class P04_JsonSchemaValidation extends SpartanTestBase {
                 .get("/api/spartans/search")
                 .then().statusCode(200)
                 .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("SearchSpartansSchema.json"));
+    }
+
+    @DisplayName("GET /api/spartans/search to validate with JsonSchemaValidator with filePath")
+    @Test
+    public void test3(){
+        given().accept(ContentType.JSON)
+                .when().get("/api/spartans/search")
+                .then().statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchema(new File("src/test/resources/SearchSpartansSchema.json")));
+    }
+
+    /**
+     *     Do schema validation for ALLSPARTANS and POST SINGLE SPARTAN
+     *
+     *     ALL SPARTANS
+     *      1- Get all spartans by using /api/spartans
+     *      2- Validate schema by using  JsonSchemaValidator
+     *
+     *
+     *    POST SINGLE SPARTANS
+     *       1- Post single spartan
+     *       2- Validate schema by using  JsonSchemaValidator
+     *
+     */
+
+
+    @Test
+    public void test4(){
+
+    given().accept(ContentType.JSON)
+            .when().get("/api/spartans")
+            .then().statusCode(200)
+            .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("AllSpartansSchema.json"));
+
+
+    }
+
+    @Test
+    public void test5(){
+        Map<String,Object> requestBody=new LinkedHashMap<>();
+        requestBody.put("name","Aykut");
+        requestBody.put("gender","Female");
+        requestBody.put("phone",1234567890);
+
+        given().accept(ContentType.JSON).and().contentType(ContentType.JSON).body(requestBody)
+                .when().post("/api/spartans")
+                .then().statusCode(201)
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("SpartanPostSchema.json"));
+
+
+
     }
 
 
